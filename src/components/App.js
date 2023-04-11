7ca1207042d0e70358a5396d34b71aedc044ac14
Urvashi <urvashi_suden@softprodigy.com>
2023-04-11 18:33:25 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,95 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
+import Main from './Main'
 import './App.css';
+import Navbar from'./Navbar.js';
+import Marketplace from '../abis/Marketplace.json';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockChainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+     await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockChainData(){
+    const web3= window.web3
+    const accounts =await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId();
+    console.log("Network Id",networkId);
+    const networkData = Marketplace.networks[networkId];
+    console.log("NETWORK DATA",networkData);
+    if(networkData){
+      const marketplace = web3.eth.Contract(Marketplace.abi,networkData.address)
+      this.setState({ marketplace});
+      console.log("marketplace",marketplace)
+      const productCount = await marketplace.methods.productCount().call()
+      console.log(productCount.toString());
+      this.setState({productCount});
+    // Load products
+    for (var i = 1; i <= productCount; i++) {
+      const product = await marketplace.methods.products(i).call()
+      this.setState({
+        products: [...this.state.products, product]
+      })
+    }
+    console.log(this.state.products);
+      this.setState({ loading: false})
+    }
+    else{
+      window.alert('Something went wrong!')
+    }
+  }
+ 
+  constructor(props){
+        super(props)
+        this.state = {
+          account: '',
+          productCount: 0,
+          products: [],
+          loading: true
+        }
+        this.createProduct = this.createProduct.bind(this);
+        this.purchaseProduct = this.purchaseProduct.bind(this);
+   }
+   
+   createProduct(name, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+    .once('receipt', (receipt) => {
+      console.log(receipt,receipt);
+      this.setState({ loading: false })
+    })
+  }
+   
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <Navbar accountNumber = {this.state.account}/>
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="http://www.dappuniversity.com/bootcamp"
@@ -16,33 +99,12 @@ class App extends Component {
             Dapp University
           </a>
         </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
+        <main role="main" className="col-lg-12 d-flex">
+  { this.state.loading
+    ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+    : <Main products={this.state.products} createProduct={this.createProduct} purchaseProduct={this.purchaseProduct}/>
+  }
+</main>
       </div>
     );
   }
